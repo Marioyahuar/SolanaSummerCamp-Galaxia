9977a1196d50686a7f025f336261fb0145907b57
Marioyahuar <marioyahuar@gmail.com>
2022-07-25 19:11:03 -0500

    Conexion Solana

--- crowfunding-solana-app/src/App.tsx
+++ crowfunding-solana-app/src/App.tsx
@@ -8,27 +8,43 @@ import Footer from './components/Footer';
 import Explore from './pages/Explore';
 import Project from './pages/Project';
 import Landing from './pages/Landing';
+import * as Web3 from '@solana/web3.js'
+import {PhantomWalletAdapter} from '@solana/wallet-adapter-phantom';
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
 
 function App() {
+  const endpoint = Web3.clusterApiUrl('devnet')
+  const wallet = new PhantomWalletAdapter()
+
   return (
-    <BrowserRouter>
-      <ThemeProvider theme={darkTheme}>
-        <Header/>
-      </ThemeProvider>
-      <ThemeProvider theme={theme}>
-        <main>
-          <Routes>
-            <Route path="/" element={< Landing />} />
-            <Route path="/explore" element={< Explore />} />
-            <Route path="/sponsoring" element={< Explore />} />
-            <Route path="/:id" element={< Project />} />
-          </Routes>
-        </main>
-      </ThemeProvider>
-      <ThemeProvider theme={darkTheme}>
-        <Footer />
-      </ThemeProvider>
-    </BrowserRouter>
+    <ConnectionProvider endpoint={endpoint}>
+            <WalletProvider wallets={[wallet]}>
+                <WalletModalProvider>
+                    <div className="App">
+                      <BrowserRouter>
+                        <ThemeProvider theme={darkTheme}>
+                          <Header/>
+                        </ThemeProvider>
+                        <ThemeProvider theme={theme}>
+                          <main>
+                            <Routes>
+                              <Route path="/" element={< Landing />} />
+                              <Route path="/explore" element={< Explore />} />
+                              <Route path="/sponsoring" element={< Explore />} />
+                              <Route path="/:id" element={< Project />} />
+                            </Routes>
+                          </main>
+                        </ThemeProvider>
+                        <ThemeProvider theme={darkTheme}>
+                          <Footer />
+                        </ThemeProvider>
+                      </BrowserRouter>
+                    </div>
+                </WalletModalProvider>
+            </WalletProvider>
+        </ConnectionProvider>
+    
   );
 }
 
