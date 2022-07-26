b3b2ad438c976fbdad1f05eb1f950e35225b3c66
Coralain <103149767+Cora42@users.noreply.github.com>
2022-07-25 19:20:51 -0500

    Project Card

--- crowfunding-solana-app/src/assets/css/theme.js
+++ crowfunding-solana-app/src/assets/css/theme.js
@@ -18,12 +18,12 @@ const typography = {
   fontSize: 14,
   h1: {
     fontFamily: 'Poppins, Nunito, ' + commonFonts,
-    fontSize: '2em',
+    fontSize: '3em',
     fontWeight: 700,
   },
   h2: {
     fontFamily: 'Poppins, Nunito, ' + commonFonts,
-    fontSize: '1.75em',
+    fontSize: '2em',
     fontWeight: 700,
   },
   h3: {
@@ -48,13 +48,17 @@ const typography = {
   },
 };
 
+const shadows = {
+  1: "0px 2px 4px 0px rgba(0,0,0,0.1), 0px -2px 2px 0px rgba(255,255,255,0.1)",
+  2: "0px 8px 12px 0px rgba(0,0,0,0.1), 0px -4px 8px 0px rgba(255,255,255,0.1)",
+}
 
 const lightPalette = {
   mode: 'light',
   primary: {
-    main: '#18ED98',
-    light: '#64FAB2',
-    dark: '#00C087',
+    main: '#00C087',
+    light: '#18ED98',
+    dark: '#00B796',
     contrastText: '#2A2843',
   },
   secondary: {
@@ -69,9 +73,22 @@ const lightPalette = {
     disabled: '#A2AFC5',
     icon: '#4D4A75',
   },
+  warning: {
+    main: '#FFEC42',
+    light: '#FFF842',
+    dark: '#FFDD42',
+    contrastText: '#2A2843',
+  },
+  success: {
+    main: '#79F09A',
+    light: '#88F890',
+    dark: '#50E78B',
+    contrastText: '#2A2843',
+  },
   divider: '#4D4A7580',
   background: {
-    paper: '#EFF1F4',
+    // paper: '#EFF1F4',
+    paper: '#F9F9FB',
     default: '#F9F9FB',
   }
 }
@@ -98,7 +115,8 @@ const darkPalette = {
   },
   divider: '#D6DDE880',
   background: {
-    paper: '#2A2843',
+    // paper: '#2A2843',
+    paper: '#1E1C35',
     default: '#1E1C35',
   }
 }
@@ -107,8 +125,10 @@ const darkPalette = {
 export const theme = createTheme({
   palette: lightPalette,
   typography: typography,
+  shadows: shadows,
 });
 export const darkTheme = createTheme({
   palette: darkPalette,
   typography: typography,
+  shadows: shadows,
 });
---
+++ crowfunding-solana-app/src/components/ProjectCard.tsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import { ProjectMin } from '../models/Project'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Card, CardActionArea, CardContent, CardMedia, Chip, Typography } from '@mui/material'
+import { faCheck, faClock, faEllipsis, faUserGroup } from '@fortawesome/free-solid-svg-icons'
+import moment from 'moment'
+import Progress from './Progress'
+
+function ProjectCard( p: ProjectMin ) {
+  return (
+    <Card className='project-card'>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          image={p.imgUrl ?? "images/project_img.png"}
+          alt={p.imgAlt ?? "project image"}
+        >
+          {/* {p.imgUrl ? '' : p.name.split(" ").map(s=>s[0]).join('') } */}
+        </CardMedia>
+        <CardContent className='column'>
+          <div>
+            <Typography gutterBottom variant="h3" component="h5" align='center'>
+              {p.name}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {p.description}
+            </Typography>
+          </div>
+          <Progress reached={p.solRaised} goal={p.solGoal} />
+          <Typography className="row">
+            <span className='row'>
+              <FontAwesomeIcon icon={faClock} />
+              <strong>
+                { moment(p.dateLimit).diff(moment(), 'days') }
+              </strong>
+              days left.
+            </span>
+            |
+            <span className='row'>
+              <FontAwesomeIcon icon={faUserGroup} />
+              <strong>{p.qPatrons}</strong>
+              Patrons
+            </span>
+          </Typography>
+          { (p.rewards !== undefined && p.rewards.length > 0) ? 
+            <ul className='column c-short'>
+              {p.rewards.map((r,i)=>{return(
+                <li className='row'>
+                  <FontAwesomeIcon icon={r.complete ? faCheck : faEllipsis} />
+                  <Typography className='f-fill' component='span'>{r.name}</Typography>
+                  <Chip size="small"
+                    label={r.complete?'DONE':'WAITING'}
+                    color={r.complete?"success":"warning"} />
+                </li>
+              )})}
+            </ul>
+          : <></>}
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  )
+}
+
+export default ProjectCard
