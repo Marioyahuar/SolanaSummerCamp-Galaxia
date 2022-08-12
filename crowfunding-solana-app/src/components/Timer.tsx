efa1fcf07f32f2f73ed59570ba26bf5a43168d05
Coralain <103149767+Cora42@users.noreply.github.com>
2022-08-12 12:55:06 -0500

    timer: difftime fixed???

--- crowfunding-solana-app/src/components/Timer.tsx
+++ crowfunding-solana-app/src/components/Timer.tsx
@@ -1,22 +1,48 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faClock } from '@fortawesome/free-solid-svg-icons'
-import moment from 'moment'
+import moment, { duration } from 'moment'
 import { Paper, Stack, Typography } from '@mui/material'
 
-function Timer( p: {dateLimit: Date} ) {
+interface Duration {
+  dd: number,
+  hh: number,
+  mm: number,
+  ss: number,
+}
 
-  const dateLimit = moment(p.dateLimit);
-  const [diffTime , setDiffTime] = React.useState(
-    moment.duration( dateLimit.diff(new Date(), 'milliseconds') )
-  );
-  const handleTick = (event: React.SyntheticEvent) => {
-    let msLeft = dateLimit.diff(new Date(), 'milliseconds');
-    setDiffTime( moment.duration(msLeft) );
+function Timer( p: {dateLimit: Date} ) {
+  
+  const msDateLimit = p.dateLimit.getTime();
+  const [timeToDay , setTimeToDay] = React.useState({ dd:0, hh:0, mm:0, ss:0});
+
+  const getTimeDiff = () => {
+    let now = new Date().getTime();
+    let timeDiff : number = msDateLimit - now;
+    console.log(p.dateLimit, msDateLimit, now, timeDiff);
+    setTimeToDay({
+      dd: Math.floor( timeDiff / 1000 / 60 / 60 / 24 ),
+      hh: Math.floor((timeDiff / 1000 / 60 / 60) % 24),
+      mm: Math.floor((timeDiff / 1000 / 60) % 60),
+      ss: Math.floor((timeDiff / 1000) % 60)
+    })
   };
+
   React.useEffect(()=>{
-    setInterval(handleTick, 1000);
-  }, [diffTime.asMilliseconds() > -1]);
+    setInterval(getTimeDiff, 1000);
+  }, [timeToDay.dd > -2]);
+
+  // const dateLimit = moment(p.dateLimit);
+  // const [diffTime , setDiffTime] = React.useState(
+  //   moment.duration( dateLimit.diff(new Date(), 'milliseconds') )
+  // );
+  // const handleTick = (event: React.SyntheticEvent) => {
+  //   let msLeft = dateLimit.diff(new Date(), 'milliseconds');
+  //   setDiffTime( moment.duration(msLeft) );
+  // };
+  // React.useEffect(()=>{
+  //   setInterval(handleTick, 1000);
+  // }, [diffTime.asMilliseconds() > -1]);
 
   return (
     <div className='timer border-gradient'>
@@ -24,7 +50,7 @@ function Timer( p: {dateLimit: Date} ) {
       <div className="row">
         <span className='f-fill'>
           Goal date: &nbsp;
-          <strong>{dateLimit.format("YYYY/MM/DD")}</strong>
+          <strong>{moment(p.dateLimit).format("YYYY/MM/DD")}</strong>
         </span>
         <Typography color="text.secondary" className="row">
           <FontAwesomeIcon icon={faClock} />
@@ -33,26 +59,26 @@ function Timer( p: {dateLimit: Date} ) {
       </div>
 
       {
-        diffTime.asDays() < 0 ?
+        timeToDay.dd < 0 ?
         <Typography variant="h3">
           Time over!
         </Typography>
         :
         <Stack direction="row" spacing={1} flexWrap="nowrap">
           <Paper elevation={2}>
-            <Typography variant="h3" component="span">{ diffTime.asDays().toFixed() }</Typography>
+            <Typography variant="h3" component="span">{ timeToDay.dd }</Typography>
             <Typography variant="body2" color="text.secondary">days</Typography>
           </Paper>
           <Paper elevation={2}>
-            <Typography variant="h3" component="span">{ diffTime.hours() }</Typography>
+            <Typography variant="h3" component="span">{ timeToDay.hh }</Typography>
             <Typography variant="body2" color="text.secondary">hours</Typography>
           </Paper>
           <Paper elevation={2}>
-            <Typography variant="h3" component="span">{ diffTime.minutes() }</Typography>
+            <Typography variant="h3" component="span">{ timeToDay.mm }</Typography>
             <Typography variant="body2" color="text.secondary">mins.</Typography>
           </Paper>
           <Paper elevation={2}>
-            <Typography variant="h3" component="span">{ diffTime.seconds() }</Typography>
+            <Typography variant="h3" component="span">{ timeToDay.ss }</Typography>
             <Typography variant="body2" color="text.secondary">secs.</Typography>
           </Paper>
         </Stack>
